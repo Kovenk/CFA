c1c7b7fbe46bf46febbd024df56801001f67c0a5
Kovenk <adio67@hotmail.fr>
2019-07-17 17:01:34 +0200

    a

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Stagiaire;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin")
@@ -21,4 +27,109 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+
+
+
+    
+    /**
+    * @Route("/addStagiaire", name="stagiaire_add") 
+    */
+    public function addStagiaire(Request $request, ObjectManager $manager){
+
+        $stagiaire = new Stagiaire();
+
+        $form = $this->createFormBuilder($stagiaire)
+        ->add('prenom',TextType::class)
+        ->add('nom',TextType::class)
+        ->add('dateNaissance',DateType::class)
+        ->add('telephone',TextType::class)
+        ->add('mail',TextType::class)
+        ->add('adresse',TextType::class)
+        ->add('ville',TextType::class)
+        ->add('codePostal',TextType::class)
+
+        ->add('Valider',SubmitType::class)
+        
+        ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()){    
+            $manager->persist($stagiaire);
+            $manager->flush();
+            return $this->redirectToRoute("stagiaire_index");
+        }
+        
+        return $this->render('stagiaire/add.html.twig',[
+            'form' => $form->createView()
+        ]);
+
+    }
+
+
+    /** 
+    * @Route("/stagiaire/{id}/delete", name="stagiaire_delete")
+    */
+    public function deleteStagiaire(Stagiaire $stagiaire, ObjectManager $manager){
+
+        $manager->remove($stagiaire);
+        $manager->flush();
+
+        return $this->redirectToRoute("stagiaire_index");
+
+    }
+
+    /**
+    * @Route("/addFormateur", name="formateur_add") 
+    */
+    public function addFormateur(Request $request, ObjectManager $manager){
+
+        $formateur = new Formateur();
+
+        $form = $this->createFormBuilder($formateur)
+        ->add('prenom',TextType::class)
+        ->add('nom',TextType::class)
+        ->add('dateNaissance',DateType::class)
+        ->add('telephone',TextType::class)
+        ->add('mail',TextType::class)
+        ->add('adresse',TextType::class)
+        ->add('ville',TextType::class)
+        ->add('codePostal',TextType::class)
+
+        ->add('Valider',SubmitType::class)
+        
+        ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()){    
+            $manager->persist($formateur);
+            $manager->flush();
+            return $this->redirectToRoute("formateur_index");
+        }
+        
+        return $this->render('formateur/add.html.twig',[
+            'form' => $form->createView()
+        ]);
+
+    }
+
+
+    /** 
+    * @Route("/stagiaire/{id}/delete", name="stagiaire_delete")
+    */
+    public function deleteStagiaire(Stagiaire $stagiaire, ObjectManager $manager){
+
+        $manager->remove($stagiaire);
+        $manager->flush();
+
+        return $this->redirectToRoute("stagiaire_index");
+
+    }
+
+
 }
+
--- src/Controller/StagiaireController.php
+++ src/Controller/StagiaireController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Stagiaire;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 /**
 * @Route("/stagiaire")
@@ -35,8 +40,5 @@ class StagiaireController extends AbstractController
             'stagiaire' => $stagiaire
         ]);
     }
-
-
-
 
 }
